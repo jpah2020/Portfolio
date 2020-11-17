e85405a825dd1c31c2d487bbdb7f67d0c34515ae
Rusky <jpah.design@gmail.com>
2020-11-17 00:58:45 -0500

    Updates

---
+++ precache-manifest.3d233aaa1558322b531596697aa84b4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb440daeb43b0aa7851d9a65756ccdd7",
+    "url": "/Jeimmy/index.html"
+  },
+  {
+    "revision": "59ae162919c6931d61b5",
+    "url": "/Jeimmy/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/Jeimmy/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Jeimmy/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ae162919c6931d61b5",
+    "url": "/Jeimmy/static/js/main.8eb79a20.chunk.js"
+  },
+  {
+    "revision": "26b4935764d6afef119a",
+    "url": "/Jeimmy/static/js/runtime-main.5f5915b7.js"
+  }
+]);
